80239a89eb60e31d635767d665cedf3621c9c335
Jon Scheiding <jonscheiding@gmail.com>
2021-08-01 11:01:23 -0400

    Generating PDF from images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import fs from 'fs';
+import PDFDocument from 'pdfkit';
 
 const SESSION_VARS = process.env.SESSION_VARS;
 
@@ -27,7 +28,14 @@ const SESSION_VARS = process.env.SESSION_VARS;
  */
 
 /**
+ * @typedef {object} DownloadedPage
+ * @property {number} number
+ * @property {string} data
+ */
+
+/**
  * @param {string} sessionInfo
+ * @return {DownloadedPage[]}
  */
 async function loadPages(sessionInfo) {
   const pageDataResponse = await axios.post(
@@ -40,9 +48,14 @@ async function loadPages(sessionInfo) {
   /** @type {PageData} */
   const pageData = pageDataResponse.data.d;
 
-  const pageCount = pageData.Scripts[0].PageCount;
+  const pageCount = Math.min(pageData.Scripts[0].PageCount, 10);
 
-  for (let i = 1; i <= Math.min(pageCount, 10); i++) {
+  /** @type {DownloadedPage[]} */
+  const pages = [];
+
+  for (let i = 1; i <= pageCount; i++) {
+    console.log(`Downloading page ${i} of ${pageCount}`);
+
     const singlePageResponse = await axios.post(
         'http://ep.mylines.com/BrowseScript.aspx/LoadSinglePage',
         {
@@ -54,10 +67,33 @@ async function loadPages(sessionInfo) {
     const singlePage = singlePageResponse.data.d;
 
     for (const page of singlePage.Pages) {
-      fs.writeFileSync(`./temp/page${page.PageNum}.png`,
-          page.EncodedFile, {encoding: 'base64'});
+      pages.push({
+        number: page.PageNum,
+        data: page.EncodedFile,
+      });
     }
   }
+
+  return pages;
 }
 
-loadPages().catch(console.error);
+/**
+ * @param {DownloadedPage[]} pages
+ */
+async function generatePdf(pages) {
+  const pdf = new PDFDocument({autoFirstPage: false});
+  pdf.pipe(fs.createWriteStream('./temp/result.pdf'));
+
+  pages = pages.sort((a, b) => a.number - b.number);
+
+  for (const page of pages) {
+    const buffer = Buffer.from(page.data, 'base64');
+    const image = pdf.openImage(buffer);
+    pdf.addPage({size: [image.width, image.height]});
+    pdf.image(image, 0, 0);
+  }
+
+  pdf.end();
+}
+
+loadPages().then(generatePdf).catch(console.error);
